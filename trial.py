39ee46c21148b0e152d04d896f96ea611eb9bfd1
Mathenge-Alex <arxmaina@gmail.com>
2022-04-28 04:01:25 +0300

    add url functionality to index.html

--- trial.py
+++ trial.py
@@ -5,28 +5,35 @@ from newsapi import NewsApiClient
 
 
 app = Flask(__name__)
+# ....................................................................................
 
+# BBC News 
 @app.route("/")
 def index():
     newsapi = NewsApiClient(api_key="c46a57bef5c04ec0a493acd9c7a46218")
     newsHeadlines = newsapi.get_top_headlines(sources="bbc-news")
+    anchors = newsapi.get_top_headlines(sources="bbc-news")
 
     myArticles = newsHeadlines["articles"]
 
     news = []
     about = []
     thumbnail = []
+    link = []
 
     for item in range(len(myArticles)):
         theArticles = myArticles[item]
         news.append(theArticles["title"])
         about.append(theArticles["description"])
         thumbnail.append(theArticles["urlToImage"])
+        link.append(theArticles["url"])
 
-    stories = zip(news, about, thumbnail)
+    stories = zip(news, about, link, thumbnail)
     return render_template("index.html", context=stories)
 
+# ....................................................................................
 
+# CNN News 
 @app.route('/cnn')
 def cnn():
     newsapi = NewsApiClient(api_key="c46a57bef5c04ec0a493acd9c7a46218")
@@ -50,6 +57,28 @@ def cnn():
 
 
 # ....................................................................................
+
+# Google News
+
+@app.route('/google')
+def google():
+    newsapi = NewsApiClient(api_key="c46a57bef5c04ec0a493acd9c7a46218")
+    newsHeadlines = newsapi.get_top_headlines(sources="google-news")
+
+    myArticles = newsHeadlines["articles"]
+
+    news = []
+    about = []
+    thumbnail = []
+
+    for item in range(len(myArticles)):
+        theArticles = myArticles[item]
+        news.append(theArticles["title"])
+        about.append(theArticles["description"])
+        thumbnail.append(theArticles["urlToImage"])
+
+    stories = zip(news, about, thumbnail)
+    return render_template("google.html", context=stories)
 
 if __name__ == "__main__":
     app.run(debug=True)
