71792a17060090ceea5600fec431fe34f9178d34
Mathenge-Alex <arxmaina@gmail.com>
2022-04-28 03:40:46 +0300

    add CNN news page

--- trial.py
+++ trial.py
@@ -24,8 +24,32 @@ def index():
         thumbnail.append(theArticles["urlToImage"])
 
     stories = zip(news, about, thumbnail)
-    # title = "Al-jazeera Latest news"
     return render_template("index.html", context=stories)
+
+
+@app.route('/cnn')
+def cnn():
+    newsapi = NewsApiClient(api_key="c46a57bef5c04ec0a493acd9c7a46218")
+    newsHeadlines = newsapi.get_top_headlines(sources="cnn")
+
+    myArticles = newsHeadlines["articles"]
+
+    news = []
+    about = []
+    thumbnail = []
+
+    for item in range(len(myArticles)):
+        theArticles = myArticles[item]
+        news.append(theArticles["title"])
+        about.append(theArticles["description"])
+        thumbnail.append(theArticles["urlToImage"])
+
+    stories = zip(news, about, thumbnail)
+    return render_template("cnn.html", context=stories)
+
+
+
+# ....................................................................................
 
 if __name__ == "__main__":
     app.run(debug=True)
