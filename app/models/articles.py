fdba74e181afeff87839646521a5a3e4772f11cc
Mathenge-Alex <arxmaina@gmail.com>
2022-05-03 19:47:28 +0300

    feat: add articles class

---
+++ app/models/articles.py
@@ -0,0 +1,13 @@
+class Articles:
+    '''
+    method to get all the articles
+    '''        
+    def __init__(self,news, about, link, thumbnail, time):
+        '''
+        method to display the objects
+        '''
+        self.news=news
+        self.about=about
+        self.link=link
+        self.thumbnail=thumbnail
+        self.time=time
