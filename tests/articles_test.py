d2dbac873c9fa712f42ead4a987b819de97e5f95
Mathenge-Alex <arxmaina@gmail.com>
2022-05-04 09:34:14 +0300

    feat: add news and articles test files.

---
+++ tests/articles_test.py
@@ -0,0 +1,26 @@
+import unittest
+from app.models.articles import Articles
+
+class TestArticles(unittest.TestCase):
+    '''
+    Test class that defines test cases for the articles class behaviors.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_article =  Articles("","","","","") # create an articles object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_article.title,"")
+        self.assertEqual(self.new_article.description,"")
+        self.assertEqual(self.new_article.urlToImage,"")
+        self.assertEqual(self.new_article.publishedAt,"")
+        self.assertEqual(self.new_article.url,"")
---
+++ tests/sources_test.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models.news import News
+
+
+
+class SourcesTest(unittest.TestCase):
+
+    '''
+    Test Class to test the Sources class behavior
+    '''
+
+    # def setUp(self):
+    #     '''
+    #     Set up method to run before every Test
+    #     '''
+    #     self.new_source = News("cnn", "CNN", "description", "url")
+
+    # def test_instance(self):
+    #     self.assertTrue(isinstance(self.new_source, News, description, url))
