e7868abc4775b42633d03b18ef3f709f38f4afdd
Mathenge-Alex <arxmaina@gmail.com>
2022-05-04 09:32:27 +0300

    refact: refactor application run code.

--- app/models/articles.py
+++ app/models/articles.py
@@ -2,12 +2,12 @@ class Articles:
     '''
     method to get all the articles
     '''        
-    def __init__(self,news, about, link, thumbnail, time):
+    def __init__(self,title, description, url, urlToImage, publishedAt):
         '''
         method to display the objects
         '''
-        self.news=news
-        self.about=about
-        self.link=link
-        self.thumbnail=thumbnail
-        self.time=time
+        self.title=title
+        self.description = description
+        self.url=url
+        self.urlToImage=urlToImage
+        self.publishedAt=publishedAt
