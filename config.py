b3fe8f0c833af8a5fc12be70b26bd295dbc9cb73
Mathenge-Alex <arxmaina@gmail.com>
2022-05-04 13:26:24 +0300

    refact: refactor views code in manage.py

--- config.py
+++ config.py
@@ -1,35 +1,35 @@
-import os
+# import os
 
 
-class Config:
-    '''
-    General configuration parent class
-    '''
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
     
-    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+#     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     pass
 
 
-class DevConfig(Config):
-    '''
-    Development  configuration child class
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
 
-    DEBUG = False
+#     DEBUG = False
 
-config_options={
-    'development': DevConfig,
-    'production': ProdConfig
-}
+# config_options={
+#     'development': DevConfig,
+#     'production': ProdConfig
+# }
