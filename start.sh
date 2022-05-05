a8900ff2ad68038ef03b711f198779d2f50da065
Mathenge-Alex <arxmaina@gmail.com>
2022-05-05 15:02:06 +0300

    fix: fix api_key bug

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export SECRET_KEY = 'zander1234'
 
 
-export NEWS_API_KEY = '41697c571e2e41a4a9adbab4a0d1dcb9'
+export NEWS_API_KEY = '490d370d771345e6bce1aee547e7578e'
 
 
 python3.9 manage.py serve
